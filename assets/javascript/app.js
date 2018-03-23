75679151ebb26aa54a8d2578bbff2fc60014c2ea
Leslie Rose <leslierose@Leslies-MBP.attlocal.net>
2018-03-23 11:51:52 -0500

    points system updating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,6 +11,7 @@ var gameOn = false;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unAnswered = 0;
+var questionAsked = false;
 var questions = ["To get over Richard, what did Monica start making?", "What was the name of the self help book that the girls loved?", "Where was the 'Aroma' room?", "What was wrong with the couch Ross returned to the store?", "What was the name of Eddie's ex-girlfriend?", "How many long-stemmed roses did Ross send to Emily?", "What was Phoebe in charge of at Rachel's suprise party?", "How many lasagnas did Monica make for her aunt?", "What heirloom did Phoebe inherit?"];
 var choices = [
     ["Pancakes", "Marmalade", "Jam", "Candy"],
@@ -41,66 +42,71 @@ var count = 0;
 //Run "startSlideshow" when we click the "start" button.
 var showChoices;
 var startChoices;
-var secondsTimer = 6;
-
+var secondsTimer = 16;
 
 console.log(choices[0][3]);
 
 function startGame () {
-    console.log(choices);
     gameOn = true;
-
     $("#start").click(startQuestions);
+}
 
-      function startQuestions() {
+function startQuestions() {
         $("#start").hide();
         displayQuestion();
         //Use showQuestions to hold the setInterval to run nextQuestion.
-        showQuestions = setInterval(nextQuestion, 5000);
-      }
+        showQuestions = setInterval(nextQuestion, 15000);
 
-      function displayQuestion() {
+    function displayQuestion() {
         $("#questions").text(questions[count]);
-        $("#choices").html("<div class='choicestyling'>" + choices[count][0] + "<br>"  + choices[count][1] + "<br>" +choices[count][2] + "<br>" + choices[count][3] + "<br>");
-        console.log(choices[count]);
-            //"<div id='option" + [count] + "'" + '<p>' + '<h4>' + choices[count]);
-    //   choiceSelections();
-        var secondsInterval = setInterval(countdownTimer, 1000);           
-               function countdownTimer() {
-                secondsTimer--;
-                $("#seconds").text("Time Remaining: " + secondsTimer + "Seconds");
-                if (secondsTimer <= 0) {
-                    clearInterval(secondsInterval);
-                    secondsTimer = 5;
-                   }}}
+        console.log(questions[count]);
+        $("#choices").html("<div class='choicestyling'>" + "<button id='choice1'>" + choices[count][0] + "</button>" + "<br>"  + "<button id='choice2'>" + choices[count][1] + "</button>" + "<br>" + "<button id='choice3'>" + choices[count][2] + "</button>" + "<br>" + "<button id='choice4'>" + choices[count][3] + "</button>" + '</div>');
+        questionAsked = true;
+        questionPoints();
 
-      function nextQuestion() {
-        //  Increment the count by 1.
-        count++;
-        //Use a setTimeout to run displayQuestion after 1 second.
-        setTimeout(displayQuestion, 1000);
-        //If the count is the same as the length of the question array, reset the count to 0.
-        if (count === questions.length && choices.length) {
-          count = 0;
-        }
-      }
+    var secondsInterval = setInterval(countdownTimer, 1000);           
+    function countdownTimer() {
+    secondsTimer--;
+    $("#seconds").text("Time Remaining: " + secondsTimer + "Seconds");
+    if (secondsTimer <= 1) {
+        clearInterval(secondsInterval);
+        secondsTimer = 15;
+        }}}
 
-     
+    function nextQuestion() {
+    //  Increment the count by 1.
+    count++;
+    //Use a setTimeout to run displayQuestion after 1 second.
+    setTimeout(displayQuestion, 1000);
+    //If the count is the same as the length of the question array, reset the count to 0.
+    if (count === questions.length && choices.length) {
+        count = 0;
+    }
+    }
+
       function stopQuestion() {
         //Put our clearInterval here:
         clearInterval(showQuestions); 
-      }        
-    }
-
+      }}      
+    
+      function questionPoints() {
+        if (questionAsked) {
+        $("#choice3").on( "click", function() {       
+        if (questions[count] === questions[0] && ($("#choice3")))  {
+            correctAnswers++
+            console.log(correctAnswers);
+        } })
+        $("#choice1, #choice2, #choice4").on( "click", function() {
+        if (questions[count] === questions[0] && ($("#choice1, #choice2, #choice4"))){
+            correctAnswers--;
+            incorrectAnswers++;
+            console.log(correctAnswers);
+            console.log(incorrectAnswers);
+        }
+    }) 
+}}
 
     
-
-
-
-
-
-
-    // renderChoices();
     startGame();
-    //choiceSelections();
+    questionPoints();
    
